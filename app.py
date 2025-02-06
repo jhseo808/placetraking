679115f03d4c8d18a81e33907d466433ea5e3a4e
서정호 | JH$EO <121909004+jhseo808@users.noreply.github.com>
2025-02-06 22:19:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ chromedriver_autoinstaller.install()
 
 driver = webdriver.Chrome()
 
-# 1.네이버 검색창 + 키워드로 드라이버 get
+# 1.네이버 검색창 + 키워드
 search_query = "여의도 맛집"
 serch_link = f"https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query={search_query}"
 driver.get(serch_link)
@@ -27,13 +27,28 @@ except:
     
 # 3.찾으려는 업체를 ID 기반으로 찾기
 time.sleep(2)
-a_id = "1332012839"
+a_id = "1332012839" # 신홍러우 여의도점  ID
 a_id_select = f"a[href*='/{a_id}?entry=pll']"
+
+# 3-2.없으면 인피니티 스크롤 5회 시도
 a_elements = driver.find_elements(By.CSS_SELECTOR, a_id_select)
 
 a_element = random.choice(a_elements)
-targer_a_element = a_element.find_element(By.XPATH,'./..')
-
-# 3-2.없으면 인피니티 스크롤 5회 시도
+for _ in range(5):
+    targer_a_element = a_element.find_element(By.XPATH,'./..')
+    tagname = targer_a_element.get_attribute("tagName")
+    if tagname == "LI":
+        print("LI의 태그를 찾았습니다.")
+        break
+    a_element = targer_a_element
+    
+all_list_select =  "#_list_scroll_container > div > div > div.place_business_list_wrapper > ul > LI"
+all_list_element = driver.find_elements(By.CSS_SELECTOR, all_list_select)
+rank = 1
+for uni_element in all_list_element:
+    if uni_element == targer_a_element:
+        break
+    rank += 1
+print(f"{search_query} // 현재 {rank}등에 노출되고 있습니다.")
 # 4.여러 키워드, 여러 업체의 순위를 찾는 프로그램으로 변경
 input()
